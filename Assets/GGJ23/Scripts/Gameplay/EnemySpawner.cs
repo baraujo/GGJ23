442df86def4c939c8700dad7c12572a85f89824c
Bruno Santos Araújo <bruno@baraujo.net>
2023-02-01 16:53:33 -0400

    Adding workaround for SplineAnimate runtime instantiation, 2 waves configured

--- Assets/GGJ23/Scripts/Gameplay/EnemySpawner.cs
+++ Assets/GGJ23/Scripts/Gameplay/EnemySpawner.cs
@@ -46,6 +46,7 @@ namespace GGJ23.Gameplay
                     {
                         // Game won!
                         Debug.Log("Game won!");
+                        m_IsRunning = false;
                     }
                     else
                     {
@@ -62,7 +63,7 @@ namespace GGJ23.Gameplay
                 {
                     if (m_Elapsed > m_CurrentWave.m_DelayBetweenEnemies)
                     {
-                        EnemyController enemy = Instantiate(m_NormalEnemyPrefab, transform).GetComponent<EnemyController>();
+                        EnemyController enemy = Instantiate(m_NormalEnemyPrefab, transform.position, Quaternion.identity, transform).GetComponent<EnemyController>();
                         enemy.SplineRef.splineContainer = m_SplineRef;
                         enemy.SplineRef.Play();
                         m_CurrentWave.m_NormalQuantity--;
@@ -73,7 +74,7 @@ namespace GGJ23.Gameplay
                 {
                     if (m_Elapsed > m_CurrentWave.m_DelayBetweenBosses)
                     {
-                        EnemyController enemy = Instantiate(m_BossEnemyPrefab, transform).GetComponent<EnemyController>();
+                        EnemyController enemy = Instantiate(m_BossEnemyPrefab, transform.position, Quaternion.identity, transform).GetComponent<EnemyController>();
                         enemy.SplineRef.splineContainer = m_SplineRef;
                         enemy.SplineRef.Play();
                         m_CurrentWave.m_BossQuantity--;
