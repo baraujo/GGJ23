09cd3b16bc91a523499316aa28ee514ce408b75b
Bruno Santos Araújo <bruno@baraujo.net>
2023-02-01 17:14:04 -0400

    Adding external triggers for wave advance

--- Assets/GGJ23/Scripts/Gameplay/EnemySpawner.cs
+++ Assets/GGJ23/Scripts/Gameplay/EnemySpawner.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Splines;
 
@@ -19,9 +20,10 @@ namespace GGJ23.Gameplay
         [SerializeField] private GameObject m_BossEnemyPrefab;
         [SerializeField] private WaveParameters[] m_Waves;
         [SerializeField] private float m_DelayBetweenWaves;
-        [SerializeField] private float m_Elapsed;
+        [SerializeField] private float m_InitialWaveDelay;
         [SerializeField] private SplineContainer m_SplineRef;
 
+        private float m_Elapsed = 0;
         private bool m_WaveActive = false;
         private int m_WaveIndex = -1;
         private bool m_IsRunning = false;
@@ -29,35 +31,15 @@ namespace GGJ23.Gameplay
 
         void Start()
         {
-            m_Elapsed = 0;
             m_IsRunning = true;
+            StartCoroutine(TriggerNextWaveRoutine(m_InitialWaveDelay));
         }
 
         void Update()
         {
             if (!m_IsRunning) return;
 
-            if (!m_WaveActive)
-            {
-                if (m_Elapsed > m_DelayBetweenWaves)
-                {
-                    m_WaveIndex++;
-                    if (m_WaveIndex >= m_Waves.Length)
-                    {
-                        // Game won!
-                        Debug.Log("Game won!");
-                        m_IsRunning = false;
-                    }
-                    else
-                    {
-                        m_WaveActive = true;
-                        m_CurrentWave = m_Waves[m_WaveIndex];
-                        m_Elapsed = 0;
-                        Debug.Log($"Starting wave {m_WaveIndex}");
-                    }
-                }
-            }
-            else
+            if(m_WaveActive)
             {
                 if (m_CurrentWave.m_NormalQuantity > 0)
                 {
@@ -86,10 +68,34 @@ namespace GGJ23.Gameplay
                     m_WaveActive = false;
                     Debug.Log($"Ending wave {m_WaveIndex}");
                     m_Elapsed = 0;
+                    StartCoroutine(TriggerNextWaveRoutine(m_DelayBetweenWaves));
                 }
             }
 
             m_Elapsed += Time.deltaTime;
+        }
+
+        private IEnumerator TriggerNextWaveRoutine(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            NextWave();
+        }
+
+        public void NextWave()
+        {
+            m_WaveIndex++;
+            if (m_WaveIndex >= m_Waves.Length)
+            {
+                Debug.Log("Game won!");
+                m_IsRunning = false;
+            }
+            else
+            {
+                m_WaveActive = true;
+                m_CurrentWave = m_Waves[m_WaveIndex];
+                m_Elapsed = 0;
+                Debug.Log($"Starting wave {m_WaveIndex}");
+            }
         }
     }
 }
