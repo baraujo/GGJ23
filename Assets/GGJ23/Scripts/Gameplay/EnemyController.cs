01e7417c60785b6543720090a2c48685991107e3
Bruno Santos Araújo <bruno@baraujo.net>
2023-02-05 11:49:42 -0400

    Add damage flash to enemies

--- Assets/GGJ23/Scripts/Gameplay/EnemyController.cs
+++ Assets/GGJ23/Scripts/Gameplay/EnemyController.cs
@@ -1,4 +1,6 @@
 using GGJ23.Utils;
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Splines;
 using UnityEngine.UI;
@@ -9,6 +11,7 @@ namespace GGJ23.Gameplay
     {
         [SerializeField] private Image m_HealthBarImage = null;
         [SerializeField] private GameplayManagerRef m_GameplayManagerRef = null;
+        [SerializeField] private SpriteRenderer m_Sprite;
 
         private SplineAnimate m_SplineAnimate;
         private HealthManager m_HealthManager;
@@ -25,6 +28,7 @@ namespace GGJ23.Gameplay
             m_SplineAnimate = GetComponent<SplineAnimate>();
             m_HealthManager = GetComponent<HealthManager>();
             m_ObjectiveLayer = LayerMask.GetMask("EnemyObjective");
+            m_Sprite = GetComponentInChildren<SpriteRenderer>();
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
@@ -40,6 +44,19 @@ namespace GGJ23.Gameplay
         public void TakeDamage()
         {
             m_HealthBarImage.fillAmount = (float)m_HealthManager.CurrentHealth / m_HealthManager.InitialHealth;
+            StartCoroutine(DamageFlashRoutine());
+        }
+
+        private IEnumerator DamageFlashRoutine()
+        {
+            Color c = m_Sprite.color;
+            m_Sprite.color = Color.red;
+            yield return new WaitForSeconds(0.05f);
+            m_Sprite.color = c;
+            yield return new WaitForSeconds(0.05f);
+            m_Sprite.color = Color.red;
+            yield return new WaitForSeconds(0.05f);
+            m_Sprite.color = c;
         }
 
         public void Die()
